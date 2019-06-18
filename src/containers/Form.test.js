96e95c026e5604a06fcf9ba4d0245ecda60042a8
Guillaume Drouen <gdrouen@siege.edpsciences.net>
2019-06-18 16:00:56 +0200

    feat: add test and package.json url

--- src/containers/Form.test.js
+++ src/containers/Form.test.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Form from "./Form";
 import Posted from "../components/Posted";
+import Input from "../components/Input";
 import { shallow } from "enzyme";
 import { findByProp } from "../tests/utils";
 
-describe("Form", () => {
+describe("Form display", () => {
 	const setup = props => shallow(<Form {...props} />);
 
 	it("displays the form on startup", () => {
@@ -12,9 +13,31 @@ describe("Form", () => {
 		expect(findByProp(wrapper, "formWrapper").find("form")).toBeTruthy();
 	});
 
-	it("display the message while form has been submitted", () => {
+	it("display the message when form has been submitted", () => {
 		const wrapper = setup();
 		wrapper.setState({ formSubmission: true });
 		expect(findByProp(wrapper, "formWrapper").find(<Posted />)).toBeTruthy();
+	});
+
+	it("displays the inputs", () => {
+		const wrapper = setup();
+		wrapper.setState({
+			form: {
+				firstname: {
+					name: "firstname",
+					title: "First name",
+					type: "text",
+					value: "",
+					placeholder: "Enter your first name",
+					validation: {
+						required: true,
+						valid: false,
+						touched: false
+					}
+				}
+			}
+		});
+		console.log(findByProp(wrapper, "formWrapper").debug());
+		expect(findByProp(wrapper, "formWrapper").find(Input)).toBeTruthy();
 	});
 });
